8d9c6f83f00e5484f913a0dc9d13f1e9ae5ad0ce
AlessEnex <154332929+AlessEnex@users.noreply.github.com>
2025-01-14 11:49:26 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+import { createRow } from "./table.js";
+import { exportToCSV } from "./csv.js";
+import { openOutlookWithEmail } from "./email.js";
+
+const JSON_URL = "data.json";
+const SUPPLIERS_URL = "suppliers.json";
+
+const fetchData = async (url) => {
+    const response = await fetch(url);
+    return await response.json();
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const data = await fetchData(JSON_URL);
+    const suppliers = await fetchData(SUPPLIERS_URL);
+
+    createRow(data, suppliers);
+
+    document.getElementById("generaCSV").addEventListener("click", exportToCSV);
+    document.getElementById("generaEmail").addEventListener("click", openOutlookWithEmail);
+});
